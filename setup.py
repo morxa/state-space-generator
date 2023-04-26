f58654f98013086cb30c4783b34dbca88bfd9cc3
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-26 23:55:03 +0200

    added manifest.in

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.install import install as install
 
 
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 HERE = Path(__file__).resolve().parent
 
 
