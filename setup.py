db727a52055273d428343f2a1fd6c1352416b162
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-29 14:58:39 +0200

    updated wheel

--- setup.py
+++ setup.py
@@ -1,48 +1,22 @@
 import os
 import subprocess
-import shutil
-from pathlib import Path
 
-from setuptools import setup, Extension, find_packages
-from setuptools.command.install import install as install
+from setuptools import setup
 from setuptools.command.build_py import build_py
 
 
-__version__ = "0.1.4"
-
-
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-    class bdist_wheel(_bdist_wheel):
-
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
-
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't link with python ABI, but require python3
-            python, abi = 'py3', 'none'
-            return python, abi, plat
-except ImportError:
-    bdist_wheel = None
+__version__ = "0.1.5"
 
 
 class CustomBuild(build_py):
     def run(self):
+        """ Builds the scorpion binaries. """
         curr_dir = os.getcwd()
-        print(curr_dir)
-
-        os.chdir('state_space_generator/scorpion')
-        print(os.getcwd())
-
+        os.chdir(os.path.join(curr_dir, 'state_space_generator/scorpion'))
         subprocess.check_call(["python", "build.py"])
-
         os.chdir(curr_dir)
-
         super().run()
+
 
 setup(
     name="state_space_generator",
@@ -54,20 +28,16 @@ setup(
     description="A tool for state space exploration of PDDL files",
     long_description="",
     packages=["state_space_generator"],
+    # package_data is copied after build, manifest is for source distribution.
     package_data={"state_space_generator":
         ["scorpion/fast-downward.py",
          "scorpion/README.md",
          "scorpion/LICENSE.md",
          "scorpion/builds/release/bin/*",
-         "scorpion/builds/release/bin/translate/*",
-         "scorpion/builds/release/bin/translate/pddl/*",
-         "scorpion/builds/release/bin/translate/pddl_parser/*",
          "scorpion/driver/*"]
     },
     cmdclass={
-        'bdist_wheel': bdist_wheel,
         'build_py': CustomBuild},
-    has_ext_modules=lambda: True,  # to not obtain pure python wheels
-    include_package_data=True,  # To copy the package data after build
+    include_package_data=True,
     zip_safe=False,  # contains platform specific code
 )
